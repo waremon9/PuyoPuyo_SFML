b8571dc540dfbc6dd0ca7644d09c7bee40b891d4
Thomas Verhille <verhille.thomas@orange.fr>
2021-05-02 02:16:07 +0200

    Trying to get all sprite right

--- PuyoPuyo_SFML/Puyo.cpp
+++ PuyoPuyo_SFML/Puyo.cpp
@@ -1,18 +1,30 @@
 #include "Puyo.h"
+#include "Util.h"
 
 Puyo::Puyo()
 {
 	GridCoordinate = sf::Vector2f(0, 0);
+	State = PuyoState::Neutral;
+	Color = PuyoColor::Red;
 }
 
 Puyo::Puyo(int x, int y)
 {
 	GridCoordinate = sf::Vector2f(x, y);
+	State = PuyoState::Neutral;
+	Color = randomColor();
 }
 
 Puyo::Puyo(sf::Vector2f coord)
 {
 	GridCoordinate = coord;
+	State = PuyoState::Neutral;
+	Color = randomColor();
+}
+
+PuyoColor Puyo::randomColor()
+{
+	return PuyoColor(RandomInt(0, static_cast<int>(PuyoColor::Count)));
 }
 
 void Puyo::Tick(float DeltaTime)
--- PuyoPuyo_SFML/Puyo.h
+++ PuyoPuyo_SFML/Puyo.h
@@ -1,16 +1,13 @@
 #pragma once
 #include "Entity.h"
 #include "SFML/Graphics/VertexBuffer.hpp"
+#include <map>
 
-enum class PuyoColor
-{
-	Red,
-	Green,
-	Blue,
-	Yellow,
-	Purple,
-	Pink,
-	Cyan
+enum class PuyoColor{
+	Red, Gree, Blue, Yellow, Purple, Pink, Cyan, Garbage, Count
+};
+enum class PuyoState {
+	Neutral, S, N, SN, E, SE, NE, SNE, W, SW, NW, SNW, EW, SEW, NEW, NSEW, Death1, Body, Eyes, Death2, Count
 };
 
 class Puyo : Entity
@@ -18,7 +15,9 @@ class Puyo : Entity
 private:
 	sf::Vector2f GridCoordinate;
 	PuyoColor Color;
+	PuyoState State;
 
+	PuyoColor randomColor();
 public:
 	Puyo();
 	Puyo(int x, int y);
@@ -26,4 +25,3 @@ public:
 
 	void Tick(float DeltaTime) override;
 };
-
